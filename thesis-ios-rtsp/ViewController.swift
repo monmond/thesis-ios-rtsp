d96b6c10c1cd8e8c90c5d65c97899336bdfc2e30
sasawat sankosik <aimangtub.mond@gmail.com>
2019-11-13 16:31:05 +0700

    Initial Commit

---
+++ thesis-ios-rtsp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thesis-ios-rtsp
+//
+//  Created by Sasawat Sankosik on 13/11/2562 BE.
+//  Copyright © 2562 ssankosik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
